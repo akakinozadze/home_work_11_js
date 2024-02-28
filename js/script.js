01e9a4831db574108b67ffcf7ca8a6296e97e41a
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2024-02-28 23:21:17 +0400

    pagination

--- js/script.js
+++ js/script.js
@@ -10,6 +10,11 @@ const splide = new Splide(".splide", {
   interval: 3000,
   start: 3,
   speed: 500,
+  classes: {
+    pagination: "splide__pagination akaki1",
+    page: "splide__pagination__page akaki2",
+  },
 });
 
-splide.mount();
+splide.mount()
+
